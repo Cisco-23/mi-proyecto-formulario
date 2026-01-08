b3f354f83a3ecdedd1a4a2f41a1cd3c8a69d9f16
Cisco-23 <153534945+Cisco-23@users.noreply.github.com>
2026-01-08 11:03:26 +0100

    Add files via upload

---
+++ src/app.spec.ts
@@ -0,0 +1,60 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { App } from './app';
+import { MachineService } from './services/machine.service';
+import { of } from 'rxjs';
+
+import { PRODUCTS_MOCK, Product } from './model/product.data';
+
+/*class MockVendingService {
+  getProducts() {
+    const copiaSegura = PRODUCTS_MOCK.map((p) => ({ ...p }));
+    return of(copiaSegura);
+  }
+}
+  */
+
+describe('AppComponent', () => {
+  let component: App;
+  let fixture: ComponentFixture<App>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+      providers: [{ provide: MachineService }],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  /*
+  it('should load real products on init', () => {
+    expect(component.products().length).toBe(3);
+    expect(component.products()[0].name).toBe('Coca-cola');
+  });
+
+  it('should buy a product correctly (Coca-cola)', () => {
+    const product = component.products()[0];
+
+    component.addCoin(2);
+
+    component.selectProduct(product);
+
+    expect(component.balance()).toBe(1.2);
+    expect(component.dispensedProduct()).toBe(product.name);
+  });
+
+  it('should not buy if out of stock', () => {
+    const products = component.products();
+    products[0].stock = 0;
+    component.products.set(products);
+
+    component.addCoin(5);
+    component.selectProduct(products[0]);
+
+    expect(component.dispensedProduct()).toBe('');
+    expect(component.balance()).toBe(5);
+  });
+
+  */
+});
